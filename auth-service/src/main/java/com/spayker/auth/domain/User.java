4bd0fc4c9ac4981bf5c8610139b3d4266d8b72dc
spayker <spykerstar@gmail.com>
2020-01-29 13:57:25 +0100

    fixed auth data source setup

--- auth-service/src/main/java/com/spayker/auth/domain/User.java
+++ auth-service/src/main/java/com/spayker/auth/domain/User.java
@@ -1,63 +1,85 @@
 package com.spayker.auth.domain;
 
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.userdetails.UserDetails;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.util.Date;
 
-import javax.persistence.*;
-import java.util.List;
-
-//@Document(collection = "users")
 @Entity
-@Table(name = "User", uniqueConstraints = @UniqueConstraint(columnNames = {"userName"}, name = "USER_UNIQUE_USERNAME"))
-public class User implements UserDetails {
+@Table(name = "users")
+public class User {
 
 	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	@Column(name = "ID", columnDefinition = "bigint unsigned")
+	@GeneratedValue(strategy = GenerationType.SEQUENCE)
+	private long id;
+
+	private String name;
+
+	@Column(unique = true)
 	private String username;
 
 	private String password;
 
-	@Override
-	public String getPassword() {
-		return password;
+	private boolean enabled = true;
+
+	private Date lastLogin;
+
+	public User() { }
+
+	public User(String name, String email, String password) {
+		this.username = email;
+		this.name = name;
+		this.password = password;
 	}
 
-	@Override
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
 	public String getUsername() {
 		return username;
-	}
-
-	@Override
-	public List<GrantedAuthority> getAuthorities() {
-		return null;
 	}
 
 	public void setUsername(String username) {
 		this.username = username;
 	}
 
+	public String getPassword() {
+		return password;
+	}
+
 	public void setPassword(String password) {
 		this.password = password;
 	}
 
-	@Override
-	public boolean isAccountNonExpired() {
-		return true;
-	}
-
-	@Override
-	public boolean isAccountNonLocked() {
-		return true;
-	}
-
-	@Override
-	public boolean isCredentialsNonExpired() {
-		return true;
-	}
-
-	@Override
 	public boolean isEnabled() {
-		return true;
+		return enabled;
+	}
+
+	public void setEnabled(boolean enabled) {
+		this.enabled = enabled;
+	}
+
+	public Date getLastLogin() {
+		return lastLogin;
+	}
+
+	public void setLastLogin(Date lastLogin) {
+		this.lastLogin = lastLogin;
 	}
 }
--- auth-service/src/main/java/com/spayker/auth/repository/UserRepository.java
+++ auth-service/src/main/java/com/spayker/auth/repository/UserRepository.java
@@ -1,10 +1,26 @@
 package com.spayker.auth.repository;
 
+import java.util.Date;
+import java.util.List;
+
 import com.spayker.auth.domain.User;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 @Repository
-public interface UserRepository extends CrudRepository<User, String> {
+public interface UserRepository extends CrudRepository<User, Long> {
+
+    User findByUsername(String username);
+
+    List<User> findByName(String name);
+
+    @Query("UPDATE User u SET u.lastLogin=:lastLogin WHERE u.username = ?#{ principal?.username }")
+    @Modifying
+    @Transactional
+    void updateLastLogin(@Param("lastLogin") Date lastLogin);
 
 }
